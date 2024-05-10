93a60bd6690d14d8236d699fd670e8b550ddbf18
Arnur <arnursabet@gmail.com>
2024-05-10 16:14:46 +0500

    Working MVP

---
+++ chainlit-backend/app.py
@@ -0,0 +1,70 @@
+import os
+from fastapi import FastAPI
+from openai import AsyncOpenAI
+
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from chainlit.auth import create_jwt
+from chainlit.server import app
+import chainlit as cl
+
+
+client = AsyncOpenAI(api_key=os.environ["OPENAI_API_KEY"])
+
+settings = {
+    "model": "gpt-3.5-turbo",
+    "temperature": 0,
+    "max_tokens": 500,
+    "top_p": 1,
+    "frequency_penalty": 0,
+    "presence_penalty": 0,
+}
+
+app = FastAPI()
+
+# Set up CORS middleware
+origins = [
+    "http://localhost:5173",  # Adjust the origins as per your frontend setup
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/custom-auth")
+async def custom_auth():
+    # Verify the user's identity with custom logic.
+    token = create_jwt(cl.User(identifier="Test User"))
+    # print("Token: ", token)
+    return JSONResponse({"token": token})
+
+@cl.on_chat_start
+async def on_chat_start():
+    cl.user_session.set(
+        "message_history",
+        [{"role": "system", "content": "You are a helpful Kazakhstan legal assistant. You always reply in Russian."}],
+    )
+    await cl.Message(content="Connected to Chainlit!").send()
+
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    message_history = cl.user_session.get("message_history")
+    message_history.append({"role": "user", "content": message.content})
+
+    msg = cl.Message(content="")
+    await msg.send()
+
+    stream = await client.chat.completions.create(
+        messages=message_history, stream=True, **settings
+    )
+
+    async for part in stream:
+        if token := part.choices[0].delta.content or "":
+            await msg.stream_token(token)
+
+    message_history.append({"role": "assistant", "content": msg.content})
+    await msg.update()
---
+++ frontend/src/App.tsx
@@ -0,0 +1,48 @@
+import { useEffect } from "react";
+
+import {
+  ChainlitAPI,
+  sessionState,
+  useChatSession,
+} from "@chainlit/react-client";
+import { Playground } from "./components/playground";
+import { useRecoilValue } from "recoil";
+import { PwCNavbar } from "./components/ui/navbar";
+
+const CHAINLIT_SERVER = "http://0.0.0.0:8000";
+const userEnv = {};
+
+const apiClient = new ChainlitAPI(CHAINLIT_SERVER, "app");
+
+function App() {
+  const { connect } = useChatSession();
+  const session = useRecoilValue(sessionState);
+  useEffect(() => {
+    if (session?.socket.connected) {
+      return;
+    }
+    //connect({wsEndpoint: CHAINLIT_SERVER, userEnv});
+    fetch(apiClient.buildEndpoint("/custom-auth"))
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log("Data: ", data)
+        connect({
+          client: apiClient,
+          userEnv,
+          accessToken: `Bearer: ${data.token}`,
+        });
+      });
+  }, [connect]);
+
+  return (
+    <>
+      <div>
+        <Playground />
+      </div>
+    </>
+  );
+}
+
+export default App;
